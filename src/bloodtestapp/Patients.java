3ef0dab9b8ffc29c5445858100eb981404fad109
JC2244 <cannon.joy@hotmail.com>
2025-03-18 14:34:24 +0000

    Third Commit
    Added Patients Class
    Declared Variables
    Constructor
    Getters
    Setters

---
+++ src/bloodtestapp/Patients.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bloodtestapp;
+
+/**
+ *
+ * @author canno
+ */
+public class Patients {
+    //Declare Variables
+    private String name;
+    private int age;
+    private String priority;
+    private String gpDetails;
+
+    public Patients(String name, int age, String priority, String gpDetails) {
+        this.name = name;
+        this.age = age;
+        this.priority = priority;
+        this.gpDetails = gpDetails;
+    }
+    
+    //Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public String getGpDetails() {
+        return gpDetails;
+    }
+
+    //Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public void setGpDetails(String gpDetails) {
+        this.gpDetails = gpDetails;
+    }
+    
+    
+    
+}
